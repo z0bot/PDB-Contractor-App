111f4f7313bed71c556a31f4896072fed7334dc5
iotarex <z_series@tuta.io>
2020-10-20 18:32:00 -0500

    Added first commit

---
+++ app/src/main/java/com/palodurobuilders/contractorapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.palodurobuilders.contractorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageButton mAddProjectButton;
+    ImageButton mArchiveDrawerButton;
+    Button mLogoutButton;
+    FrameLayout mHouseSelectionFrame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setStatusBarColor();
+
+        mAddProjectButton = findViewById(R.id.button_add_house);
+        mArchiveDrawerButton = findViewById(R.id.button_archive_drawer);
+        mLogoutButton = findViewById(R.id.button_logout);
+        mHouseSelectionFrame = findViewById(R.id.frame_house_selection);
+
+        mLogoutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                testToast();
+            }
+        });
+    }
+
+    private void testToast()
+    {
+        Toast toast = Toast.makeText(this, R.string.toast_test, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void setStatusBarColor()
+    {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#FFFFFF"));
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
