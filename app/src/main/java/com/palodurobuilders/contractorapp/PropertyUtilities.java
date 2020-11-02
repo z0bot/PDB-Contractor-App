aeb9f016905df4d360996ec866fb9e8cb2e862ba
iotarex <z_series@tuta.io>
2020-11-01 19:58:47 -0600

    Fixed UI Scaling on smaller screened android phones.

--- app/src/main/java/com/palodurobuilders/contractorapp/PropertyUtilities.java
+++ app/src/main/java/com/palodurobuilders/contractorapp/PropertyUtilities.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.util.Objects;
