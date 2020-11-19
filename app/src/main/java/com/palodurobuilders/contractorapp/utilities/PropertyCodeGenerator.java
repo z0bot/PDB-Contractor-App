a34f371013f8c6a357094e7d837a06dc153dcaf5
iotarex <z_series@tuta.io>
2020-11-19 12:10:44 -0600

    Made a class to generate our random property IDs, but forgot to make a branch for it and I'm too lazy to change it right now.

---
+++ app/src/main/java/com/palodurobuilders/contractorapp/utilities/PropertyCodeGenerator.java
@@ -0,0 +1,20 @@
+package com.palodurobuilders.contractorapp.utilities;
+
+import java.security.SecureRandom;
+
+public class PropertyCodeGenerator
+{
+    static final String DIGIT_DICTIONARY = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
+    static SecureRandom random = new SecureRandom();
+
+
+    public static String generatePropertyCode()
+    {
+        StringBuilder stringBuilder = new StringBuilder(6);
+        for(int i = 0; i < 6; i++)
+        {
+            stringBuilder.append(DIGIT_DICTIONARY.charAt(random.nextInt(DIGIT_DICTIONARY.length())));
+        }
+        return stringBuilder.toString();
+    }
+}
